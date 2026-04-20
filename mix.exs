bdab0a0a858d8635d2ca23d7f95123d7002c9ff1
dl-alexandre <166029845+dl-alexandre@users.noreply.github.com>
2026-04-20 12:16:00 -0700

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule AppleMusicAPI.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @source_url "https://github.com/dl-alexandre/apple_music_api"
 
   def project do
